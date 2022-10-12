5c44d8174fa12e06f9ea3bbf6fbf3fceeb0f1296
Kim Baekjung <zvgandam@gmail.com>
2022-10-12 16:45:32 +0900

    refactor: Workspace 구역 나누기

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import useSWR from 'swr';
 import { Navigate } from 'react-router-dom';
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+import gravatar from 'gravatar';
 
 type Props = {
   children?: React.ReactNode;
@@ -24,7 +36,22 @@ const Workspace: FC<Props> = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'mp' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Slack</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
--- pages/Channel/index.tsx
+++ pages/Channel/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Channel = () => {
   return (
     <Workspace>
-      <div>로그인</div>
+      <div>채널채널채널</div>
     </Workspace>
   );
 };
